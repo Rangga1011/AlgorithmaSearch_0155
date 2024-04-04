ac565d5aea37cb7b3632fbf2b1e1dfa3cbe84a66
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-04-04 11:22:09 +0700

    Menambahkan void linear search step 6

--- AlgorithmaSearch_0155/AlgorithmaSearch_0155.cpp
+++ AlgorithmaSearch_0155/AlgorithmaSearch_0155.cpp
@@ -43,7 +43,11 @@ void LinearSearch()
         i = 0; // step 2
         while (i < n) // step 3
         {
-
+            ctr++;
+            if (arr[i] == item)
+            {
+                cout << "\n" << item << " found at position" << (i + 1) << endl; // step 6
+            }
         }
     }
 }
@@ -73,8 +77,4 @@ void LinearSearch()
 
 
 
-int main()
-{
-    
-}
 
