b299bf662b3a8304047f5c23a0d6f388f23a5c72
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-04-04 10:41:48 +0700

    Menambahkan void input while

--- AlgorithmaSearch_0155/AlgorithmaSearch_0155.cpp
+++ AlgorithmaSearch_0155/AlgorithmaSearch_0155.cpp
@@ -5,6 +5,15 @@ int arr[20];  // array to searched
 int n;        // Number of element in the array
 int i;        // Index of array element
 
+void input()
+{
+    while (true)
+    {
+        cout << "Enter the number of elements in the array: ";
+        cin >> n;
+    }
+}
+
 
 
 
